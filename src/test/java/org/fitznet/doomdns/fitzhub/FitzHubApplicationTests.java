87362d9ddf19b7b42c4955b4eb3472ef59124554
Matthew Fitzgerald <mattlol85@gmail.com>
2022-08-03 00:24:58 -0400

    Init Commit

---
+++ src/test/java/org/fitznet/doomdns/fitzhub/FitzHubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fitznet.doomdns.fitzhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitzHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
